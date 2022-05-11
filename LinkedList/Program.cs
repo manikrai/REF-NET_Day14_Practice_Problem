2596575cf45d8f957ee22f1851170a6402b91727
manikrai <86585495+manikrai@users.noreply.github.com>
2022-05-12 00:11:51 +0530

    Add files via upload

---
+++ LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Data_Structures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Linked_List List=new Linked_List();
+           List.Add(56);
+            List.Add(30);
+            List.Add(70);
+             List.Display();
+            //int a = List.Search(30);
+           
+          // List.InsertAtParticularPosition(2, 30);
+            
+            //List.Display();
+        }
+    }
+}
