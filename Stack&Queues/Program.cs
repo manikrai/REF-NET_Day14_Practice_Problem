76e9f9df1aac040ba82457f829cb982bf098618a
manikrai <86585495+manikrai@users.noreply.github.com>
2022-05-12 00:16:13 +0530

    Add files via upload

---
+++ Stack&Queues/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace QueueUsingLinkedList
+{
+    public class Program
+    {
+        
+       
+        public static void Main(string[] args)
+        {
+            
+            LinkedListQueue List = new LinkedListQueue();
+            List.Enqueue(56);
+            List.Enqueue(30);
+            List.Enqueue(70);
+            
+            //Removing Elements From Queue
+            //Console.WriteLine(que.Dequeue);
+          
+            List.Display();
+
+           
+           
+        }
+    }
+}
